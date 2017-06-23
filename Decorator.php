38d04d7b0f428648837e6b14b7b4a7ac477482d8
Jonathon Wallen <jonathon@monkii.com>
2017-06-23 14:36:23 +1000

    Initial commit, skeleton files

---
+++ Decorator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Jonathon Wallen
+ * @date 23/6/17
+ * @time 2:34 PM
+ * @copyright 2008 - present, Monkii Digital Agency (http://monkii.com.au)
+ */
+
+namespace MonkiiBuilt\LadPageSectionWysiwyg;
+
+
+class Decorator {
+
+}
---
+++ resources/views/wysiwyg.blade.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @author Jonathon Wallen
+ * @date 23/6/17
+ * @time 2:34 PM
+ * @copyright 2008 - present, Monkii Digital Agency (http://monkii.com.au)
+ */
+?>
